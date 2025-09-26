7619deebc3785fe86f77aa1bfa19c9fca2085c43
martin <martincharles07@gmail.com>
2025-09-26 17:02:53 -0500

    fix get config magic

--- packages/xreal_one_driver/src/proto/net/get_config.rs
+++ packages/xreal_one_driver/src/proto/net/get_config.rs
@@ -3,7 +3,7 @@ use crate::proto::net::{protos, NetworkTransaction};
 pub struct GetConfig;
 
 impl NetworkTransaction for GetConfig {
-    const MAGIC: [u8; 2] = [0x27, 0x1d];
+    const MAGIC: [u8; 2] = [0x27, 0x1f];
     type RequestArgs = protos::get_config::Request;
     type Response = protos::get_config::Response;
 }
