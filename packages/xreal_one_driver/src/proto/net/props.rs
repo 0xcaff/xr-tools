435430d95f1aa582ff1da7236c1cf700887c6f72
martin <martincharles07@gmail.com>
2026-04-28 17:19:06 -0500

    Accept field-1 tags in PropertyResponse parser

--- packages/xreal_one_driver/src/proto/net/props.rs
+++ packages/xreal_one_driver/src/proto/net/props.rs
@@ -91,7 +91,9 @@ impl<V: PropertyValueRead> ProtobufRead for PropertyResponse<V> {
             bail!("unexpected end of stream");
         };
 
-        if tag != ((4 << 2) | V::WIRE_TYPE as u32) {
+        let expected_field_1_tag = (1 << 3) | V::WIRE_TYPE as u32;
+        let expected_field_2_tag = (2 << 3) | V::WIRE_TYPE as u32;
+        if tag != expected_field_1_tag && tag != expected_field_2_tag {
             bail!("unexpected tag: 0x{:x}", tag);
         }
 
