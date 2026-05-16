d0a2ef4b1d84d10fff6dfadfc7f8a8a2c25b5d59
martin <martincharles07@gmail.com>
2026-05-16 01:12:24 -0500

    only setup mcu recovery for now
    will save this for another day i guess

--- packages/xreal_one_ctl/src/commands/flash/mod.rs
+++ packages/xreal_one_ctl/src/commands/flash/mod.rs
@@ -1,16 +1,16 @@
 use clap::Subcommand;
 
 // pub mod dsp;
-pub mod mcu;
+// pub mod mcu;
 pub mod mcu_recovery;
 // pub mod pilot;
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
-    Mcu {
-        #[command(flatten)]
-        args: mcu::Args,
-    },
+    // Mcu {
+    //     #[command(flatten)]
+    //     args: mcu::Args,
+    // },
     // Dsp {
     //     #[command(flatten)]
     //     args: dsp::Args,
@@ -27,7 +27,7 @@ pub enum Command {
 
 pub async fn run(command: Command) -> Result<(), anyhow::Error> {
     match command {
-        Command::Mcu { args } => mcu::run(args).await,
+        // Command::Mcu { args } => mcu::run(args).await,
         // Command::Dsp { args } => dsp::run(args).await,
         // Command::Pilot { args } => pilot::run(args).await,
         Command::McuRecovery { args } => mcu_recovery::run(args).await,
